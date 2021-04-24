730e3051d0792d760462f14c37cd3f67a1bcf2b1
Lionyu <779625444@qq.com>
2021-04-25 00:43:55 +0800

    修改 WatchEffect

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -29,22 +29,24 @@ export default {
     const indicator = ref < HTMLDivElement > (null)
     const container = ref < HTMLDivElement > (null)
 
-    onMounted(() => {
-      watchEffect(() => {
-        const {
-          width
-        } = selectedItem.value.getBoundingClientRect()
-        indicator.value.style.width = width + 'px'
-        const {
-          left: left1
-        } = container.value.getBoundingClientRect()
-        const {
-          left: left2
-        } = selectedItem.value.getBoundingClientRect()
-        const left = left2 - left1
-        indicator.value.style.left = left + 'px'
-      })
-    })
+onMounted(() => {
+  watchEffect(() => {
+    const {
+      width
+    } = selectedItem.value.getBoundingClientRect()
+    indicator.value.style.width = width + 'px'
+    const {
+      left: left1
+    } = container.value.getBoundingClientRect()
+    const {
+      left: left2
+    } = selectedItem.value.getBoundingClientRect()
+    const left = left2 - left1
+    indicator.value.style.left = left + 'px'
+  }, {
+    flush: 'post'
+  })
+})
 
     const defaults = context.slots.default()
     defaults.forEach((tag) => {
