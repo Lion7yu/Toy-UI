1a98cc6441f55eda9af476b5288de827fc7e5a2d
Lionyu <779625444@qq.com>
2021-04-24 17:11:56 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import "./lib/lion.scss";
+import "./index.scss";
+import { createApp } from "vue";
+import App from "./App.vue";
+import { router } from "./router";
+import './lib/svg.js'
+import 'github-markdown-css'
+import Markdown from './components/Markdown.vue';
+
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
+app.component("Markdown", Markdown)
---
+++ src/shims-vue.d.ts
@@ -0,0 +1,9 @@
+declare module '*.vue' {
+  import { ComponentOptions } from 'vue'
+  const componentOptions: ComponentOptions
+  export default componentOptions
+}
+declare module '*.md' {
+  const str : String
+  export default str
+}
