8f50562fe8c62eee16be03037971b2e0ad6c7d4c
Lionyu <779625444@qq.com>
2021-08-01 16:11:01 +0800

    完成基本样式

--- src/lib/Rate/Rate.vue
+++ src/lib/Rate/Rate.vue
@@ -1,15 +1,44 @@
 <template>
   <div class="rate-wrapper">
-    
+    <span
+      v-for="rateNum in 5"
+      :key="rateNum" 
+      :class="[
+        'iconfont i-start',
+        rateNum <= num? 'active' : ''
+      ]"
+      :style="{fontSize: size + 'px' }"
+    ></span>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Rate'
+  name: 'Rate',
+  props:{
+    num:{
+      type: Number,
+      defaule: 0
+    },
+    size:{
+      type: Number,
+      default:30
+    } 
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-  
+  .iconfont{
+    font-size: 16px;
+    color: #999;
+
+    &.i-start{
+      transition: color .3s;
+    }
+
+    &.active{
+      color: #FBAB06;
+    }
+  }
 </style>
