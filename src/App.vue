2a111ea4da79bec0ffb7464f937dd7dd950d8823
Lionyu <779625444@qq.com>
2021-10-15 15:47:59 +0800

    创建 siteHeader 并放入页面中

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
+  <SiteHeader />
   <router-view />
 </template>
 
 <script lang="ts">
 import { ref, provide } from "vue";
+import SiteHeader from "./views/SiteHeader.vue"
 import { router } from "./router";
 
 export default {
   name: "App",
+  components: {
+    SiteHeader
+  },
   setup() {
     const width = document.documentElement.clientWidth;
     const menuVisible = ref(width <= 500 ? false : true);
---
+++ src/views/SiteHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>111</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
