5d766a34e8bbf584ead03271f3af55c4131d1617
Lionyu <779625444@qq.com>
2021-10-17 11:14:40 +0800

    更新 vue 版本

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git init &&
 git add . &&
 git commit -m "update" &&
 git branch -M master &&
-git remote add origin git@github.com:lion7yu/Lion-ui-website.git &&
+git remote add origin git@github.com:lion7yu/Toy-ui-website.git &&
 git push -f -u origin master &&
 cd ..
-echo https://github.com/Lion7yu/Lion-ui-website
+echo https://github.com/Lion7yu/Toy-ui-website
