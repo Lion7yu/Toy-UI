69adc0a3e8a78d8a100be1f0a2b0594b634ce9c6
Lionyu <779625444@qq.com>
2021-10-19 00:47:16 +0800

    修改图片引入方式

--- src/components/Carousel/Carousel1.demo.vue
+++ src/components/Carousel/Carousel1.demo.vue
@@ -12,7 +12,7 @@
       dotBgColor="#000"
     >
       <CarouselItem v-for="(item, index) of carouselData" :key="index">
-        <img :src="(`/images/carouselImages/${item.img_name}`)" />
+        <img :src="item.img_name" />
       </CarouselItem>
     </Carousel>
   </div>
@@ -21,13 +21,34 @@
 <script lang="ts">
 import { Carousel } from "../../lib/index";
 import { CarouselItem } from "../../lib/index";
-import carouselData from "../../lib/Carousel/carousel";
+import xili from "/images/carouselImages/xili.png"
+import blazer from "/images/carouselImages/blazer.png"
+import mdla from "/images/carouselImages/mdla.jpg"
+import staple from "/images/carouselImages/staple.jpg"
+import milk from "/images/carouselImages/milk.png"
 export default {
   components: {
     Carousel,
     CarouselItem,
   },
   setup() {
+    const carouselData = [
+      {
+        img_name: xili,
+      },
+      {
+        img_name: blazer
+      },
+      {
+        img_name: mdla
+      },
+      {
+        img_name: staple
+      },
+      {
+        img_name: milk
+      },
+    ]
     return {
       carouselData,
     };
