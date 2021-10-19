3c67a60bacb0580d349483ef4bf76b2e9bd69b4e
Lionyu <779625444@qq.com>
2021-10-20 00:22:36 +0800

    调整样式

--- src/lib/Selector/Menu.vue
+++ src/lib/Selector/Menu.vue
@@ -1,72 +1,71 @@
 <template>
   <div class="selector-menu">
     <template v-if="searchData.length > 0">
-      <div class="menu-item" 
-      v-for="(item,index) of searchData"
-      :key="index"
-      @click="setItemValue(item)"
-      >
-    {{item.text}}
-    </div>
+      <div
+        class="menu-item"
+        v-for="(item,index) of searchData"
+        :key="index"
+        @click="setItemValue(item)"
+      >{{ item.text }}</div>
     </template>
-    <NoDataTip v-else/>
+    <NoDataTip v-else />
   </div>
 </template>
 
 <script>
 
-import {ref, onMounted,watch} from 'vue'
+import { ref, onMounted, watch } from 'vue'
 import NoDataTip from './NoDataTip.vue'
 
 export default {
-  name:'SelectorMenu',
-  components:{
+  name: 'SelectorMenu',
+  components: {
     NoDataTip
   },
-  props:{
+  props: {
     searchValue: String,
-    data:{
-      type:Array,
-      default(){
+    data: {
+      type: Array,
+      default() {
         return [
           {
-            id:1,
-            value:'1',
-            text:'选项1'
+            id: 1,
+            value: '1',
+            text: '选项1'
           },
           {
-            id:2,
-            value:'2',
-            text:'选项2'
+            id: 2,
+            value: '2',
+            text: '选项2'
           },
           {
-            id:3,
-            value:'3',
-            text:'选项3'
+            id: 3,
+            value: '3',
+            text: '选项3'
           },
         ]
       }
-    } 
+    }
   },
-  setup(props,ctx){
+  setup(props, ctx) {
     const searchData = ref([])
-    onMounted(()=>{
+    onMounted(() => {
       searchData.value = props.data
     })
-    watch(()=>{
+    watch(() => {
       return props.searchValue
-    },(value)=>{
+    }, (value) => {
       filterData(value)
     })
-    const filterData = (value) =>{
-      searchData.value = props.data.filter((item)=>{
+    const filterData = (value) => {
+      searchData.value = props.data.filter((item) => {
         return item.text.toLowerCase().includes(props.searchValue.toLowerCase())
       })
     }
     const setItemValue = (item) => {
-      ctx.emit('setItemValue',item)
+      ctx.emit('setItemValue', item)
     }
-    return{
+    return {
       setItemValue,
       searchData
     }
@@ -75,30 +74,30 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .selector-menu{
-    display: none;
-    position: absolute;
-    left: 0;
-    top: 48px;
-    width: 100%;
-    background-color: #fff;
-    box-shadow: 0 0 10px #ddd;
-    border-radius: 5px;
-    cursor: pointer;
+.selector-menu {
+  display: none;
+  position: absolute;
+  left: 0;
+  top: 48px;
+  width: 100%;
+  background-color: #fff;
+  box-shadow: 0 0 10px #ddd;
+  border-radius: 5px;
+  cursor: pointer;
+  z-index: 999;
 
-    .menu-item{
-      height: 35px;
-      text-align: center;
-      line-height: 35px;
-      font-size: 14px;
-      color: #666;
-      margin: 10px 0;
-      transition: background-color .2s linear;
+  .menu-item {
+    height: 35px;
+    text-align: center;
+    line-height: 35px;
+    font-size: 14px;
+    color: #666;
+    margin: 10px 0;
+    transition: background-color 0.2s linear;
 
-      &:hover{
-        background-color: #ededed;
-      }
+    &:hover {
+      background-color: #ededed;
     }
   }
-
+}
 </style>
