2f503800b60cb011b76d8b3c4e58a3fc85ac3690
Lionyu <779625444@qq.com>
2021-10-29 00:42:53 +0800

    重构位置表

--- src/lib/Popover/Popover.vue
+++ src/lib/Popover/Popover.vue
@@ -34,22 +34,30 @@ export default {
     const positionContent = () => {
       const { contentWrapper, triggerWrapper } = instance.ctx.$refs
       document.body.appendChild(contentWrapper)
-      let { width, height, top, left } = triggerWrapper.getBoundingClientRect()
-      if (instance.ctx.position === 'top') {
-        contentWrapper.style.left = left + window.scrollX + 'px'
-        contentWrapper.style.top = top + window.scrollY + 'px'
-      } else if (instance.ctx.position === 'bottom') {
-        contentWrapper.style.left = left + window.scrollX + 'px'
-        contentWrapper.style.top = top + height + window.scrollY + 'px'
-      } else if (instance.ctx.position === 'left') {
-        contentWrapper.style.left = left + window.scrollX + 'px'
-        let { height: height2 } = contentWrapper.getBoundingClientRect()
-        contentWrapper.style.top = top + window.scrollY + (height - height2) / 2 + 'px'
-      } else if (instance.ctx.position === 'right') {
-        contentWrapper.style.left = left + window.scrollX + width + 'px'
-        let { height: height2 } = contentWrapper.getBoundingClientRect()
-        contentWrapper.style.top = top + window.scrollY + (height - height2) / 2 + 'px'
+      const { width, height, top, left } = triggerWrapper.getBoundingClientRect()
+      const { height: height2 } = contentWrapper.getBoundingClientRect()
+      let positions = {
+        top: {
+          top: top + window.scrollY,
+          left: left + window.scrollX,
+        },
+        bottom: {
+          top: top + height + window.scrollY,
+          left: left + window.scrollX,
+        },
+        left: {
+          top: top + window.scrollY + (height - height2) / 2,
+          left: left + window.scrollX
+        },
+        right: {
+          top: top + window.scrollY + (height - height2) / 2,
+          left: left + window.scrollX + width
+        },
       }
+      contentWrapper.style.left = positions[instance.ctx.position].left + 'px'
+      contentWrapper.style.top = positions[instance.ctx.position].top + 'px'
+      contentWrapper.style.right = positions[instance.ctx.position].right + 'px'
+      contentWrapper.style.bottom = positions[instance.ctx.position].bottom + 'px'
     }
 
     let onClickDocument = (e) => {
